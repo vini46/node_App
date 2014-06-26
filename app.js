d5e659a4d6e332fe1fd9f56ae467e683632880b8
vinod baradwaj <vinod.baradwaj@live.com>
2014-06-26 11:24:40 +0000

    rework

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('/workouts', workouts.index);
 
-app.listen(3000, function(){
+/*app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+}); */
+module.exports = app;
