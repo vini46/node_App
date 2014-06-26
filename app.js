68ed1205ec87c660229383ed1ecf0081796f996e
vinod baradwaj <vinod.baradwaj@live.com>
2014-06-26 11:29:53 +0000

    rework

--- app.js
+++ app.js
@@ -1,40 +1,28 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
-  , HTTP = require( 'http')
-  , workouts = require('./routes/workouts');
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
+  , http = require('http');
+      
+var app = express();
+      
 app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
-
+      
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
   app.use(express.errorHandler());
 });
-
-// Routes
-
+      
 app.get('/', routes.index);
-app.get('/workouts', workouts.index);
-
-/*app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-}); */
-module.exports = app;
+      
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port %s in %s mode.",
+app.get('port'), app.settings.env);
+});
