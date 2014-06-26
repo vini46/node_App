1b4005aed7d1fb7e1040c3a0d717bd409b3ef2b1
vinod baradwaj <vinod.baradwaj@live.com>
2014-06-26 11:13:30 +0000

    added procfile

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , HTTP = require( 'http')
+  , workouts = require('./routes/workouts');
 
 var app = module.exports = express.createServer();
 
@@ -30,6 +32,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/workouts', workouts.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
